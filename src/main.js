c45123f066a58e62ee3a4da8f0dfa9859c0c2cc2
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-11-20 16:18:12 -0300

    3. Crie uma listagem de produtos

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-// import { fetchProductsList } from './helpers/fetchFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
+const product = document.querySelector('.products');
+
+const creatImg = async () => {
+  const lista = await fetchProductsList('computador');
+  lista.forEach((element) => {
+    const criarElemento = createProductElement(element);
+    product.appendChild(criarElemento);
+  });
+};
+creatImg();
 document.querySelector('.cep-button').addEventListener('click', searchCep);
