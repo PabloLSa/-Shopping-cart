f0418885c9fc7ad99e7943789af83e43b7ff247d
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-11-20 16:56:18 -0300

    4. Adicione um texto de carregando durante uma requisição à API

--- src/main.js
+++ src/main.js
@@ -5,12 +5,22 @@ import { createProductElement } from './helpers/shopFunctions';
 
 const product = document.querySelector('.products');
 
+const loading = () => {
+  const inner = document.createElement('p');
+  inner.className = 'loading';
+  inner.innerHTML = 'carregando...';
+  product.appendChild(inner);
+
+  return inner;
+};
 const creatImg = async () => {
+  loading();
   const lista = await fetchProductsList('computador');
   lista.forEach((element) => {
     const criarElemento = createProductElement(element);
     product.appendChild(criarElemento);
   });
+  document.querySelector('.loading').remove();
 };
 creatImg();
 document.querySelector('.cep-button').addEventListener('click', searchCep);
