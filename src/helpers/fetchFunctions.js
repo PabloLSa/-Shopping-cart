7c1ad1bc8bc933c51e1971b505282fd0af744768
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-11-20 15:28:57 -0300

    2. Implemente a função fetchProductsList

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,11 @@ export const fetchProduct = async () => {
   // seu código aqui
 };
 
-export const fetchProductsList = async () => {
-  // seu código aqui
+export const fetchProductsList = async (produto) => {
+  if (!produto) {
+    throw new Error('Termo de busca não informado');
+  }
+  const listProdutos = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${produto}`);
+  const produtoList = await listProdutos.json();
+  return produtoList.results;
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+// import { fetchProductsList } from './helpers/fetchFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
